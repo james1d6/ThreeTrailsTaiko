d717158747078cb2e2f2f2fb0d8ce15a39c6acc0
james1d6 <james2d6@gmail.com>
2017-10-09 20:54:49 -0500

    ApplicationCore (structuremap)

---
+++ ApplicationCore/IoC.cs
@@ -0,0 +1,24 @@
+﻿using StructureMap;
+
+namespace ThreeTrailsTaiko.ApplicationCore
+{
+	public static class IoC
+	{
+		private static IContainer container = new Container(c =>
+		{
+			c.Scan(s =>
+			{
+				s.TheCallingAssembly();
+				s.AssembliesFromApplicationBaseDirectory();
+				//s.IncludeNamespace("ThreeTrailsTaiko");
+				s.WithDefaultConventions();
+			});
+		});
+
+		public static T GetInstance<T>()
+		{
+				return container.GetInstance<T>();
+		}
+	}
+
+}
